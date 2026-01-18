c7663f3795e6ef73c2c90dae99feedbea872117b
Mauricio Umana <69874300+maoumas@users.noreply.github.com>
2026-01-18 16:57:39 -0500

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "SaraPuma",
+  "short_name": "SaraPuma",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#D35400",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "image_5.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "image_5.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
